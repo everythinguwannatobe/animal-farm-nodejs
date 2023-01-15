dd0a12bcce134d21d633e19b6ca9c60ba2014474
sweatdream34310 <davidchallenge1000@gmail.com>
2023-01-16 00:37:28 +0900

    Added a roaring tiger

--- test/test.js
+++ test/test.js
@@ -29,6 +29,6 @@ describe('GET', function(){
     request
     .get('/api')
     .set('Accept', 'application/json')
-    .expect(200, {"cat":"meow","dog":"bark","eel":"hiss","bear":"growl","frog":"croak","lion":"roar"}, done);
+    .expect(200, {"cat":"meow","dog":"bark","eel":"hiss","bear":"growl","frog":"croak","lion":"roar","tiger":"roar"}, done);
   })
 })
